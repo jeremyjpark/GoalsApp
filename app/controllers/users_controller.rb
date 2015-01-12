0ed4ddbed2cdc4d9db4664e96ee1fbeefffd7f23
MatthewAndJeremy <student@appacademy.io>
2015-01-12 13:21:52 -0800

    starting views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login(@user)
+      redirect_to goals_url
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email)
+  end
 end
